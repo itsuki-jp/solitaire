7f6016d1c4cb70e7a82ee87d32918722d258ec8c
itsuki-jp <itsuki19601@gmail.com>
2023-01-15 12:14:01 +0900

    stock changed from array to deque

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+class Deque {
+  constructor() {
+    this.length = 0;
+    this.front = this.back = undefined;
+  }
+  addFront(value) {
+    this.length++;
+    if (!this.front) this.front = this.back = { value };
+    else this.front = this.front.next = { value, prev: this.front };
+  }
+  removeFront() {
+    this.length--;
+    let value = this.peekFront();
+    if (this.front === this.back) this.front = this.back = undefined;
+    else (this.front = this.front.prev).next = undefined;
+    return value;
+  }
+  peekFront() {
+    return this.front && this.front.value;
+  }
+  addBack(value) {
+    this.length++;
+    if (!this.front) this.front = this.back = { value };
+    else this.back = this.back.prev = { value, next: this.back };
+  }
+  removeBack() {
+    this.length--;
+    let value = this.peekBack();
+    if (this.front === this.back) this.front = this.back = undefined;
+    else (this.back = this.back.next).back = undefined;
+    return value;
+  }
+  peekBack() {
+    return this.back && this.back.value;
+  }
+  getLength() {
+    return this.length;
+  }
+}
+
+
 class Card {
   constructor(symbol, num) {
     this.symbol = symbol;
@@ -43,33 +84,36 @@ class Board {
     this.symbols = ["♤", "♧", "♢", "♡"];
     this.nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     this.cardSize = { x: 35, y: 50 };
-    // Todo: waste削除,stockをdequeに変更
-    this.waste = []; // 山札からひっくり返されたやつ
 
-    this.stock = []; // 山札
-    this.prepareStock();
-    this.shuffle();
+    let cards = this.prepareCards();
+    this.stock = new Deque();
+    for (let i = 0; i < cards.length; i++) {
+      this.stock.addBack(cards[i]);
+    }
 
     this.tableau = []; // 段差になってるやつ
     this.prepareTableau();
 
     this.foundation = []; // Aとかがおかれるやつ
   }
-  prepareStock() {
+  prepareCards() {
+    const cards = []
     for (const symbol of this.symbols) {
       for (const num of this.nums) {
         const card = new Card(symbol, num);
         card.setPos(this.padding.x, this.padding.y);
-        this.stock.push(card);
+        cards.push(card);
       }
     }
+    this.shuffle(cards);
+    return cards;
   }
   prepareTableau() {
     let tableauLength = 7;
     for (let i = 0; i < tableauLength; i++) {
       this.tableau[i] = [];
       for (let j = 1; j <= i + 1; j++) {
-        const card = this.stock.pop();
+        const card = this.stock.removeBack();
         card.setPos(
           this.padding.x + this.spaceBetweenCard * (i + 1),
           this.padding.y + this.cardOverlap * j
@@ -84,16 +128,15 @@ class Board {
       this.foundation.push([]);
     }
   }
-  shuffle() {
-    for (let i = this.stock.length - 1; i > 0; i--) {
+  shuffle(obj) {
+    for (let i = obj.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * i + 1);
-      [this.stock[i], this.stock[j]] = [this.stock[j], this.stock[i]];
+      [obj[i], obj[j]] = [obj[j], obj[i]];
     }
   }
   output() {
-    console.log(`stock length: ${this.stock.length}`);
-    if (this.waste.length !== 0) {
-      console.log(`waste top: ${this.waste[this.waste.length - 1].getName()}`);
+    if (this.stock.getLength() !== 0) {
+      console.log(`stock top: ${this.stock.peekFront.getName()}`);
     }
     for (let i = 0; i < this.tableau.length; i++) {
       const temp = [];
@@ -109,34 +152,15 @@ class Board {
     }
   }
   isStockEmpty() {
-    return this.stock.length === 0;
+    return this.stock.getLength() === 0;
   }
-  isWasteEmpty() {
-    return this.waste.length === 0;
-  }
-  waste2Stock() {
-    for (const card of this.waste) {
-      card.setPos(this.padding.x, this.padding.y);
-      card.setFaceDown();
-      this.stock.push(card);
-    }
-    this.waste = [];
-  }
+  // Todo: 書き換える
   flip() {
-    if (this.isStockEmpty()) {
-      this.waste2Stock();
-    }
-    if (!this.isWasteEmpty) {
-      this.waste[this.waste.length - 1].setFaceDown();
-    }
-
-    const cardFromStock = this.stock.pop();
-    cardFromStock.setFaceUp();
-    cardFromStock.setPos(
-      this.padding.x,
-      this.padding.y + this.cardSize.y + this.cardOverlap
-    );
-    this.waste.push(cardFromStock);
+    const cardTop = this.stock.removeFront();
+    cardTop.setFaceDown();
+    this.stock.addBack(cardTop);
+    const cardNext = this.stock.peekFront();
+    cardNext.setFaceUp();
   }
 }
 
@@ -158,10 +182,11 @@ class Solitaire {
   }
   drawCards() {
     // waste, stock, tableau, foundation;
-    this.drawCardSet(this.Board.waste, "waste");
+    // this.drawCardSet(this.Board.stock.peekFront(), "stock");
+    this.drawCard(this.Board.stock.peekFront());
   }
   drawCardSet(cardSet, name) {
-    if (name === "waste") {
+    if (name === "stock") {
       if (cardSet.length === 0) {
         return;
       }
@@ -200,7 +225,7 @@ function test() {
   solitaire.Board.output();
   for (let i = 0; i < 100; i++) {
     console.log("------------------------------------");
-    console.log(solitaire.Board.stock[solitaire.Board.stock.length - 1]);
+    console.log(solitaire.Board.stock[solitaire.Board.stock.getLength() - 1]);
     solitaire.Board.flip();
     // solitaire.Board.output();
   }
